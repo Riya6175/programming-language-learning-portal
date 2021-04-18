a37a58013ea19ef1ce003944d6fb37ee5d0f74d0
Riya6175 <50210884+Riya6175@users.noreply.github.com>
2021-04-18 12:07:20 +0530

    Front_end files

---
+++ Project_front_end/app.js
@@ -0,0 +1,18 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.set('view engine','ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+app.listen(3000,function(){
+  console.log("server started at 3000");
+});
