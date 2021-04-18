435a61e7b70aa96f2ce5b5af3bc405d798dbd5ee
Neha Thakkar <74426577+NehaThakkar25@users.noreply.github.com>
2021-04-19 01:38:00 +0530

    Front_End Files (Format of Website)

---
+++ Project_front_end/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+app.get('/', function(req, res) {
+    res.render('index');
+    });
+
+app.get('/home', function(req, res) {
+  res.render('home');
+  });
+
+  app.get('/about', function(req, res) {
+    res.render('about');
+    });
+
+  app.get('/overview', function(req,res){
+    res.render('overview');
+  })
+  
+
+
+app.listen(3000, function(){
+  console.log("Server started at 3000.");
+})
